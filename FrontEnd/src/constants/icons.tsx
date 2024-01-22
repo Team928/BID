e84d50b3ed6b14175504ffe2ae68dd193142346d
이승현 <sh980812@naver.com>
2024-01-22 17:42:32 +0900

    feat: 상단바에 필요한 아이콘 추가 #S10P12D208-164

---
+++ FrontEnd/src/constants/icons.tsx
@@ -0,0 +1,5 @@
+export const icons = {
+  BACK: <img src="/src/assets/icon/back.png" />,
+  SEARCH: <img src="/src/assets/icon/search.png" />,
+  NOTIFY: <img src="/src/assets/icon/notify.png" />,
+};
