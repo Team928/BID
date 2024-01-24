6ff5c99de4850383c8371d41e6a7a4cb1bbb66d0
suhwa <tnghk9611@naver.com>
2024-01-24 09:23:17 +0900

    feat: 라이브 화면에 필요한 모달 구현 #S10P12D208-148
    - Modal 컴포넌트를 적용하여 구현

---
+++ FrontEnd/src/components/live/Modal/LiveSettingModal.tsx
@@ -0,0 +1,64 @@
+import Modal from '@/components/@common/Modal';
+import Toggle from '@/components/@common/Toggle';
+import { MODAL_TITLE } from '@/constants/modalTitle';
+import { useState } from 'react';
+
+export interface ILivePlusOptionFlag {
+  mask: boolean;
+  voiceModulation: boolean;
+  bgFilter: boolean;
+}
+
+const LivePlusSettingModal = ({ onClose }: { onClose: () => void }) => {
+  const [toggleFlag, setToggleFlag] = useState<ILivePlusOptionFlag>({
+    mask: false,
+    voiceModulation: false,
+    bgFilter: false,
+  });
+
+  const handleToggle = (type: keyof ILivePlusOptionFlag) => {
+    setToggleFlag(prev => {
+      return {
+        ...prev,
+        [type]: !prev[`${type}`],
+      };
+    });
+  };
+
+  return (
+    <Modal width="300px" height="auto" title={MODAL_TITLE.CAMERA_SETTING} onClose={onClose}>
+      <div className="w-full h-full">
+        <div className="px-8 py-4">
+          <div className="grid grid-cols-4 pb-3">
+            <div className="col-span-3 leading-8">화면 가면 설정</div>
+            <div className="col-span-1">
+              <Toggle isOn={toggleFlag.mask} handleToggle={() => handleToggle('mask')} />
+            </div>
+          </div>
+          <div className="grid grid-cols-4 pb-3">
+            <div className="col-span-3 leading-8">음성 변조</div>
+            <div className="col-span-1">
+              <Toggle isOn={toggleFlag.voiceModulation} handleToggle={() => handleToggle('voiceModulation')} />
+            </div>
+          </div>
+          <div className="grid grid-cols-4 pb-3">
+            <div className="col-span-3 leading-8">배경 설정</div>
+            <div className="col-span-1">
+              <Toggle isOn={toggleFlag.bgFilter} handleToggle={() => handleToggle('bgFilter')} />
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-10 px-12 mb-4">
+          <button
+            type="button"
+            className="w-full h-full rounded-3xl shadow-sm text-white text-lg bg-BID_MAIN hover:bg-BID_HOVER_MAIN "
+          >
+            적용하기
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default LivePlusSettingModal;
---
+++ FrontEnd/src/components/live/Modal/RequestSalePriceModal.tsx
@@ -0,0 +1,30 @@
+import Modal from '@/components/@common/Modal';
+import { MODAL_TITLE } from '@/constants/modalTitle';
+import ParrarelModalButtons from './ParrarelModalButtons';
+
+const RequestSalePriceModal = ({ onClose }: { onClose: () => void }) => {
+  // 현재 구매 희망가
+  const currentBuyPrice = 50000;
+
+  return (
+    <Modal width="280px" height="auto" title={MODAL_TITLE.REQUEST_SALE_PRICE} onClose={onClose}>
+      <div className="px-6 py-3">
+        {/* @TODO: input component */}
+        <div className="bg-white w-full h-12 py-1 flex items-center border-BID-GRAY">
+          <input
+            type="number"
+            placeholder="판매희망가를 입력해주세요"
+            className="px-3 h-full outline-none border w-full rounded-full"
+          ></input>
+        </div>
+        <div className="text-center text-sm">
+          현재 구매 희망가 <span className="text-BID_MAIN font-semibold">{currentBuyPrice}원</span>
+        </div>
+      </div>
+      {/* @TODO: 각 호출별 함수 로직 작성 */}
+      <ParrarelModalButtons leftText="취소" rightText="입력" handleLeft={() => onClose()} handleRight={() => null} />
+    </Modal>
+  );
+};
+
+export default RequestSalePriceModal;
