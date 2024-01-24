bad0f4da1eaecebea8af3a547e16e9c0a425b909
이승현 <sh980812@naver.com>
2024-01-24 17:47:10 +0900

    feat: 라이브 중인 경매 목록 UI 구현 #S10P12D208-200 #done

--- FrontEnd/src/components/home/SaleTab.tsx
+++ FrontEnd/src/components/home/SaleTab.tsx
@@ -1,9 +1,16 @@
-import Hot from './buy/Hot';
+import Hot from './sale/Hot';
+import Live from './sale/Live';
+import Soon from './sale/Soon';
+import DeadLine from './sale/deadLine';
 
 const SaleTab = () => {
   return (
     <div className="">
+      {/* #TODO 추후 실제 데이터로 수정해야 함 */}
       <Hot />
+      <DeadLine />
+      <Soon />
+      <Live />
     </div>
   );
 };
---
+++ FrontEnd/src/components/home/sale/Live.tsx
@@ -0,0 +1,98 @@
+import { changeEngToKr } from '@/utils/changeCategorie';
+
+const Live = () => {
+  return (
+    <>
+      <div className="py-4 border-b border-[#D9D9D9]">
+        <div className="px-BID_P">
+          <p className="font-bold">라이브 진행 중</p>
+          <p className="text-xs text-BID_BLACK">현재 라이브가 진행중입니다</p>
+        </div>
+        <div className="pl-4 py-4 flex flex-nowrap overflow-x-auto gap-2">
+          {list.map(item => {
+            return (
+              <div key={item.salePostSimpleRes.id} className="w-44 text-xs flex flex-col gap-1">
+                <div className="w-44 h-32 bg-BID_LIGHT_GRAY relative">
+                  <div className="absolute top-3 left-3 text-center bg-[#FF0000] pr-[0.4rem] pl-1 rounded-sm">
+                    <p className="text-white text-sm font-bold italic">LIVE</p>
+                  </div>
+                </div>
+                <div className="px-1 flex flex-col gap-1">
+                  <p className=" text-[0.6rem] text-BID_SUB_GRAY">{changeEngToKr(item.salePostSimpleRes.categories)}</p>
+                  <p className=" w-44 truncate whitespace-normal leading-4 line-clamp-2">
+                    {item.salePostSimpleRes.title}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Live;
+
+const list = [
+  {
+    salePostSimpleRes: {
+      id: 1,
+      title: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      categories: 'Digital',
+      createTime: '2024-01-17 15:30:00',
+      image: '',
+    },
+    immediate_price: null,
+    startTime: null,
+    startPrice: 100000,
+    endTime: '2024-01-17 15:30:00',
+    bid: 1124360,
+    status: 'ING',
+  },
+  {
+    salePostSimpleRes: {
+      id: 2,
+      title: '아이폰 삽니다~',
+      categories: 'Digital',
+      createTime: '2024-01-17 15:30:00',
+      image: '',
+    },
+    immediate_price: null,
+    startTime: null,
+    startPrice: 100000,
+    endTime: '2024-01-17 15:30:00',
+    bid: 1124360,
+    status: 'ING',
+  },
+  {
+    salePostSimpleRes: {
+      id: 3,
+      title: '아이폰 삽니다~',
+      categories: 'Digital',
+      createTime: '2024-01-17 15:30:00',
+      image: '',
+    },
+    immediate_price: null,
+    startTime: null,
+    startPrice: 100000,
+    endTime: '2024-01-17 15:30:00',
+    bid: 1124360,
+    status: 'ING',
+  },
+  {
+    salePostSimpleRes: {
+      id: 4,
+      title: '아이폰 삽니다~',
+      categories: 'Digital',
+      createTime: '2024-01-17 15:30:00',
+      image: '',
+    },
+    immediate_price: null,
+    startTime: null,
+    startPrice: 100000,
+    endTime: '2024-01-17 15:30:00',
+    bid: 1124360,
+    status: 'ING',
+  },
+];
