c26634ad1be6ee308ce4cc12bc20c5f73dfbab6b
윤희원 <gmldnjs427@gmail.com>
2024-01-25 04:59:39 +0900

    feat: Member 관련 생성 #S10P12D208-136

---
+++ BackEnd/src/main/java/com/qzp/bid/domain/member/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.qzp.bid.domain.member.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+
+
+}
---
+++ BackEnd/src/main/java/com/qzp/bid/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.qzp.bid.domain.member.repository;
+
+import com.qzp.bid.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findMemberByEmail(String email);
+}
