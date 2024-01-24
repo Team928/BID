7e7bf573d8cd732c5bba77d562f3cbf4a00191d6
윤희원 <gmldnjs427@gmail.com>
2024-01-25 05:01:43 +0900

    chore: LOGIN_SUCCESS 코드 추가 #S10P12D208-142

--- BackEnd/src/main/java/com/qzp/bid/global/result/ResultCode.java
+++ BackEnd/src/main/java/com/qzp/bid/global/result/ResultCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ResultCode {
     // Member
     REGISTER_SUCCESS(201, "회원가입에 성공하였습니다."),
+    LOGIN_SUCCESS(200, "로그인에 성공하였습니다."),
 
     //sale
     CREATE_SALE_SUCCESS(201, "판매글 생성에 성공하였습니다.");
