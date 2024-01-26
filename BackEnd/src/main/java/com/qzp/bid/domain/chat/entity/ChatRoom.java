e7f2795877fc5bf88a4c883d30a3d64d4e45bb84
최용수 <cys980220@gmail.com>
2024-01-26 16:54:47 +0900

    feat: 1대1채팅방구현 #S10P12D208-203 #done

--- BackEnd/src/main/java/com/qzp/bid/domain/chat/entity/ChatRoom.java
+++ BackEnd/src/main/java/com/qzp/bid/domain/chat/entity/ChatRoom.java
@@ -1,9 +1,34 @@
 package com.qzp.bid.domain.chat.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class ChatRoom {
 
+    @Id
+    @GeneratedValue
     private long id;
     private String roomName;
+    private long dealId;
     private String hostId;
     private String guestId;
+    private LocalDateTime time;
+
+    public ChatRoom create(Long roomId, String roomName, String hostId, String guestId) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.id = roomId;
+        chatRoom.roomName = roomName;
+        chatRoom.hostId = hostId;
+        chatRoom.guestId = guestId;
+        chatRoom.time = LocalDateTime.now();
+        return chatRoom;
+    }
+
 }
--- BackEnd/src/main/java/com/qzp/bid/domain/deal/repository/DealRepository.java
+++ BackEnd/src/main/java/com/qzp/bid/domain/deal/repository/DealRepository.java
@@ -1,8 +1,26 @@
 package com.qzp.bid.domain.deal.repository;
 
 import com.qzp.bid.domain.deal.entity.Deal;
+import com.qzp.bid.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DealRepository<T extends Deal> extends JpaRepository<T, Long> {
+
+
+    @Query("SELECT b.bidder"
+        + " FROM Deal d JOIN Bid b ON d.id = b.id"
+        + " WHERE d.id = :dealId and b.isSuccess = true")
+    Optional<Member> findBidderByDealId(
+        @Param("dealId") long dealId); // dealId로 경매 낙찰자가 있으면 테이블 반환
+
+    @Query("SELECT m"
+        + " FROM Deal d JOIN ReverseAuctionResult r ON d.id = r.id"
+        + " JOIN Member m ON r.sellerId = m.id"
+        + " WHERE d.id = :dealId")
+    Optional<Member> findSellerByDealId(
+        @Param("dealId") long dealId); // dealId로 역경매 낙찰자가 있으면 테이블 반환
 
 }
