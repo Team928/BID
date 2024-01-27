ad91a8ba9481c41cca78682a7270355820d08046
차성원 <sungwon4728@naver.com>
2024-01-27 20:11:22 +0900

    docs: sale 스웨거 설명 추가

--- BackEnd/src/main/java/com/qzp/bid/domain/deal/dto/SearchParam.java
+++ BackEnd/src/main/java/com/qzp/bid/domain/deal/dto/SearchParam.java
@@ -2,18 +2,28 @@ package com.qzp.bid.domain.deal.dto;
 
 import com.qzp.bid.domain.deal.entity.Category;
 import com.qzp.bid.domain.deal.entity.DealStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "검색 조건 파라미터")
 public class SearchParam {
 
+    @Schema(description = "페이지 번호", example = "0", defaultValue = "0", requiredMode = RequiredMode.REQUIRED)
     int page;
+    @Schema(description = "한 페이지 당 글 갯수", example = "10", defaultValue = "10", requiredMode = RequiredMode.REQUIRED)
     int size;
+    @Schema(description = "검색 카테고리 (ALL, FASSION, BEAUTY, CHILD, LIVING, DIGITAL, BOOK, TOY, PET, ETC)")
     Category catg;
+    @Schema(description = "검색 지역")
     String area;
+    @Schema(description = "최신순(desc) / 시작or마감 임박(asc)")
     String order; // 기본 최신순 desc / 마감임박 or 시작임박  asc
+    @Schema(description = "거래 진행 상태 (BEFORE, LIVE, AUCTION, END)")
     DealStatus status;
+    @Schema(description = "제목 검색 키워드")
     String keyword;
 }
--- BackEnd/src/main/java/com/qzp/bid/domain/deal/sale/controller/SaleController.java
+++ BackEnd/src/main/java/com/qzp/bid/domain/deal/sale/controller/SaleController.java
@@ -8,11 +8,15 @@ import com.qzp.bid.domain.deal.sale.dto.SaleUpdateReq;
 import com.qzp.bid.domain.deal.sale.service.SaleService;
 import com.qzp.bid.global.result.ResultCode;
 import com.qzp.bid.global.result.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,10 +27,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/deals/sales")
 @RequiredArgsConstructor
+@Tag(name = "SaleController", description = "판매/경매 api")
 public class SaleController {
 
     private final SaleService saleService;
 
+    @Operation(summary = "판매글 생성")
     @PostMapping
     public ResponseEntity<ResultResponse> createSale(@RequestBody SaleReq saleReq) {
         saleService.createSale(saleReq);
@@ -34,12 +40,14 @@ public class SaleController {
             .body(ResultResponse.of(ResultCode.CREATE_SALE_SUCCESS));
     }
 
+    @Operation(summary = "판매글 상세조회")
     @GetMapping("/{saleId}")
     public ResponseEntity<ResultResponse> getSale(@PathVariable Long saleId) {
         SaleRes saleRes = saleService.getSale(saleId);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.GET_SALE_SUCCESS, saleRes));
     }
 
+    @Operation(summary = "판매글 수정")
     @PatchMapping("/{saleId}")
     public ResponseEntity<ResultResponse> updateSale(@PathVariable Long saleId,
         @RequestBody SaleUpdateReq saleUpdateReq) {
@@ -47,14 +55,17 @@ public class SaleController {
         return ResponseEntity.ok(ResultResponse.of(ResultCode.UPDATE_SALE_SUCCESS));
     }
 
+    @Operation(summary = "판매글 삭제")
     @DeleteMapping("/{saleId}")
     public ResponseEntity<ResultResponse> deleteSale(@PathVariable Long saleId) {
         saleService.deleteSale(saleId);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.DELETE_SALE_SUCCESS));
     }
 
+    @Operation(summary = "판매글 목록 조회")
     @GetMapping
-    public ResponseEntity<ResultResponse> getSales(SearchParam searchParam) {
+    public ResponseEntity<ResultResponse> getSales(
+        @ModelAttribute @ParameterObject SearchParam searchParam) {
         SaleListPage saleListPage = saleService.getSales(searchParam);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.GET_SALE_SUCCESS, saleListPage));
     }
