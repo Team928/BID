da2c122512c341942d19a018d21c3f2e8bf67cf0
차성원 <sungwon4728@naver.com>
2024-01-29 14:00:44 +0900

    feat: swagger security 토큰 로직 추가

--- BackEnd/src/main/java/com/qzp/bid/global/config/SwaggerConfig.java
+++ BackEnd/src/main/java/com/qzp/bid/global/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.qzp.bid.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,8 +18,24 @@ public class SwaggerConfig {
             .title("BID 프로젝트 API Document")
             .version("v0.0.1")
             .description("BID 프로젝트의 API 명세서입니다.");
+
+        SecurityScheme bearer = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("Authorization")
+            .in(SecurityScheme.In.HEADER)
+            .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Authorization");
+
+        Components components = new Components()
+            .addSecuritySchemes("Authorization", bearer);
+
         return new OpenAPI()
-            .components(new Components())
+            .components(components)
+            .addSecurityItem(addSecurityItem)
             .info(info);
     }
 }
