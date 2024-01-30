f3cb67948541ae01c2cc62193873251ed8ed1d30
suhwa <tnghk9611@naver.com>
2024-01-30 17:12:00 +0900

    chore: 기타 스타일 수정 #S10P12D208-194

--- FrontEnd/src/components/live/Modal/RecordingGuideModal.tsx
+++ FrontEnd/src/components/live/Modal/RecordingGuideModal.tsx
@@ -8,10 +8,7 @@ const RecordingGuideModal = ({ onClose }: { onClose: () => void }) => {
         지금부터 라이브 녹화를 시작합니다. 가이드를 따라서 상품의 상세 정보를 보여주세요.
       </div>
       <div className="w-full h-10 px-12 mb-4">
-        <button
-          type="button"
-          className="w-full h-full rounded-3xl shadow-sm text-white text-lg bg-BID_MAIN hover:bg-BID_HOVER_MAIN "
-        >
+        <button type="button" className="blueBtn">
           시작하기
         </button>
       </div>
