714140168619d128c7fb99bd94bd0ee4d6fbf79c
차성원 <sungwon4728@naver.com>
2024-01-31 15:27:11 +0900

    feat: ImageUploader 파일 하나만 받는 메서드 추가

--- BackEnd/src/main/java/com/qzp/bid/global/util/ImageUploader.java
+++ BackEnd/src/main/java/com/qzp/bid/global/util/ImageUploader.java
@@ -11,19 +11,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.regex.Matcher;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class ImageUploader {
 
-    @Value("${image.upload.path}")
-    private String uploadPath;
+    private final String uploadPath;
+
+    public ImageUploader(@Value("${image.upload.path}") String uploadPath) {
+        this.uploadPath = uploadPath.replaceAll("/", Matcher.quoteReplacement(File.separator));
+    }
 
     public List<ImageDto> upload(List<MultipartFile> multipartFiles) {
         if (multipartFiles == null || multipartFiles.isEmpty()) {
@@ -31,27 +32,29 @@ public class ImageUploader {
         }
         List<ImageDto> imageDtos = new ArrayList<>();
 
-        String OsUploadPath = uploadPath.replaceAll("/", Matcher.quoteReplacement(File.separator));
-
         for (MultipartFile multipartFile : multipartFiles) {
-            if (!multipartFile.getContentType().startsWith("image")) {
-                throw new BusinessException(ErrorCode.IS_NOT_IMAGE);
-            }
-            String uuid = UUID.randomUUID().toString();
-            String saveFileName = uuid + "_" + multipartFile.getOriginalFilename();
-            Path savePath = Paths.get(OsUploadPath, saveFileName);
-            try {
-                Files.createDirectories(savePath.getParent());
-                multipartFile.transferTo(savePath);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            File saveFile = savePath.toFile();
-            ImageDto imageDto = new ImageDto(saveFile.getAbsolutePath(),
-                multipartFile.getOriginalFilename());
-            imageDtos.add(imageDto);
+            imageDtos.add(uploadOne(multipartFile));
         }
         return imageDtos;
+    }
+
+    public ImageDto uploadOne(MultipartFile multipartFile) {
+
+        if (!multipartFile.getContentType().startsWith("image")) {
+            throw new BusinessException(ErrorCode.IS_NOT_IMAGE);
+        }
+        String uuid = UUID.randomUUID().toString();
+        String saveFileName = uuid + "_" + multipartFile.getOriginalFilename();
+        Path savePath = Paths.get(uploadPath, saveFileName);
+        try {
+            Files.createDirectories(savePath.getParent());
+            multipartFile.transferTo(savePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        File saveFile = savePath.toFile();
+        return new ImageDto(saveFile.getAbsolutePath(),
+            multipartFile.getOriginalFilename());
     }
 
     private void removeOriginalFile(File targetFile) {
