271f4700ccaec82defe75911555675452f00d608
이승현 <sh980812@naver.com>
2024-01-31 22:11:16 +0900

    style: 주석 수정

--- FrontEnd/src/components/home/SaleTab.tsx
+++ FrontEnd/src/components/home/SaleTab.tsx
@@ -5,8 +5,7 @@ import DeadLine from './sale/DeadLine';
 
 const SaleTab = () => {
   return (
-    <div className="">
-      {/* #TODO 추후 실제 데이터로 수정해야 함 */}
+    <div>
       <Hot />
       <DeadLine />
       <SaleSoon />
--- FrontEnd/src/utils/addCommaToPrice.ts
+++ FrontEnd/src/utils/addCommaToPrice.ts
@@ -1,3 +1,4 @@
+// 가격에 콤마를 추가하는 함수
 export default function addCommaToPrice(price: number | null) {
   if (price) return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
