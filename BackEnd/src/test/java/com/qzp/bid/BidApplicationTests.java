b01f4425359ab4f6d2817e0a64dda9a76f8e2d0c
윤희원 <gmldnjs427@gmail.com>
2024-01-31 17:21:09 +0900

    fix: 테스트시 DB 없어서 빌드 안되는 것 수정2

--- BackEnd/src/test/java/com/qzp/bid/BidApplicationTests.java
+++ BackEnd/src/test/java/com/qzp/bid/BidApplicationTests.java
@@ -3,7 +3,7 @@ package com.qzp.bid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BidApplicationTests.class)
 class BidApplicationTests {
 
     @Test
