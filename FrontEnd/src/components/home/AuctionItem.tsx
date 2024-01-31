d818989c131f87264cf853716984b1dad0832598
이승현 <sh980812@naver.com>
2024-01-31 22:09:32 +0900

    fix: 경매 진행전 라이브 일정 실제 데이터로 변경 및 UI 수정

--- FrontEnd/src/components/home/AuctionItem.tsx
+++ FrontEnd/src/components/home/AuctionItem.tsx
@@ -1,6 +1,7 @@
 import { ISaleSimpleRes } from '@/types/home';
 import addCommaToPrice from '@/utils/addCommaToPrice';
 import { changeEngToKr } from '@/utils/changeCategorie';
+import { getDate } from '@/utils/getDate';
 import { getPriceName } from '@/utils/getPriceName';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,16 +10,20 @@ const AuctionItem = (props: { item: ISaleSimpleRes }) => {
   const sale = props.item.dealSimpleRes;
   const item = props.item;
 
+  const { month, date, datOfWeek, time } = getDate(sale.startTime);
+
   return (
     <div onClick={() => navigate(`/deals/${sale.id}`)} className="text-xs">
       <div className="w-32 h-32 bg-BID_LIGHT_GRAY rounded-2xl relative">
+        <img src="/src/assets/image/sample.png" className="w-full h-full rounded-2xl object-cover" />
+        {/* 경매 진행전이라면 라이브 일정 보여주기 */}
         {item.status === 'BEFORE' && (
           <>
-            <div className="w-full h-full absolute bg-black/10 rounded-2xl"></div>
-            <img src="/src/assets/image/sample.png" className="w-full h-full rounded-2xl object-cover" />
-            <div className="absolute top-9 left-8">
-              <p className="text-white font-bold text-lg ">1/5 (금)</p>
-              <p className="text-white font-bold text-2xl">10:00</p>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+              <p className="text-white font-bold text-lg whitespace-nowrap">
+                {month}/{date} ({datOfWeek})
+              </p>
+              <p className="text-white font-bold text-2xl">{time}</p>
             </div>
           </>
         )}
@@ -28,7 +33,12 @@ const AuctionItem = (props: { item: ISaleSimpleRes }) => {
         <p className="w-32 truncate whitespace-normal line-clamp-2">{sale.title}</p>
         <div className="">
           <p className="font-bold text-sm">
-            {item.status === 'BEFORE' ? addCommaToPrice(item.immediate_price) : addCommaToPrice(item.bid)}원
+            {item.status === 'BEFORE'
+              ? addCommaToPrice(item.immediatePrice)
+              : item.bid === 0
+                ? addCommaToPrice(item.startPrice)
+                : addCommaToPrice(item.bid)}
+            원
           </p>
           <p className="text-BID_SUB_GRAY">{getPriceName(item.status)}</p>
         </div>
