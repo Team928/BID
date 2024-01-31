384cc3f15b26d8e97c969cae1e3c4053d25a3397
이승현 <sh980812@naver.com>
2024-01-31 22:07:34 +0900

    feat: 라이브 중인 경매 api 통신 구현 #S10P12D208-232 #done

--- FrontEnd/src/components/home/sale/Live.tsx
+++ FrontEnd/src/components/home/sale/Live.tsx
@@ -1,6 +1,14 @@
+import { useSale } from '@/hooks/home/useSale';
 import { changeEngToKr } from '@/utils/changeCategorie';
 
 const Live = () => {
+  const { useGetSaleList } = useSale();
+  const {
+    // isLoading,
+    // error,
+    data: liveInfo,
+  } = useGetSaleList({ page: '0', size: '10', order: 'asc', status: 'LIVE' });
+
   return (
     <>
       <div className="pt-4 border-b border-[#D9D9D9]">
@@ -9,19 +17,17 @@ const Live = () => {
           <p className="text-xs text-BID_BLACK">현재 라이브가 진행중입니다</p>
         </div>
         <div className="pl-4 py-4 flex flex-nowrap overflow-x-auto gap-2">
-          {list.map(item => {
+          {liveInfo?.data.saleSimpleResList.map(item => {
             return (
-              <div key={item.salePostSimpleRes.id} className="w-44 text-xs flex flex-col gap-1">
+              <div key={item.dealSimpleRes.id} className="w-44 text-xs flex flex-col gap-1">
                 <div className="w-44 h-32 bg-BID_LIGHT_GRAY relative">
                   <div className="absolute top-3 left-3 text-center bg-[#FF0000] pr-[0.4rem] pl-1 rounded-sm">
                     <p className="text-white text-sm font-bold italic">LIVE</p>
                   </div>
                 </div>
                 <div className="px-1 flex flex-col gap-1">
-                  <p className=" text-[0.6rem] text-BID_SUB_GRAY">{changeEngToKr(item.salePostSimpleRes.categories)}</p>
-                  <p className=" w-44 truncate whitespace-normal leading-4 line-clamp-2">
-                    {item.salePostSimpleRes.title}
-                  </p>
+                  <p className=" text-[0.6rem] text-BID_SUB_GRAY">{changeEngToKr(item.dealSimpleRes.category)}</p>
+                  <p className=" w-44 truncate whitespace-normal leading-4 line-clamp-2">{item.dealSimpleRes.title}</p>
                 </div>
               </div>
             );
@@ -33,66 +39,3 @@ const Live = () => {
 };
 
 export default Live;
-
-const list = [
-  {
-    salePostSimpleRes: {
-      id: 1,
-      title: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
-      categories: 'Digital',
-      createTime: '2024-01-17 15:30:00',
-      image: '',
-    },
-    immediate_price: null,
-    startTime: null,
-    startPrice: 100000,
-    endTime: '2024-01-17 15:30:00',
-    bid: 1124360,
-    status: 'ING',
-  },
-  {
-    salePostSimpleRes: {
-      id: 2,
-      title: '아이폰 삽니다~',
-      categories: 'Digital',
-      createTime: '2024-01-17 15:30:00',
-      image: '',
-    },
-    immediate_price: null,
-    startTime: null,
-    startPrice: 100000,
-    endTime: '2024-01-17 15:30:00',
-    bid: 1124360,
-    status: 'ING',
-  },
-  {
-    salePostSimpleRes: {
-      id: 3,
-      title: '아이폰 삽니다~',
-      categories: 'Digital',
-      createTime: '2024-01-17 15:30:00',
-      image: '',
-    },
-    immediate_price: null,
-    startTime: null,
-    startPrice: 100000,
-    endTime: '2024-01-17 15:30:00',
-    bid: 1124360,
-    status: 'ING',
-  },
-  {
-    salePostSimpleRes: {
-      id: 4,
-      title: '아이폰 삽니다~',
-      categories: 'Digital',
-      createTime: '2024-01-17 15:30:00',
-      image: '',
-    },
-    immediate_price: null,
-    startTime: null,
-    startPrice: 100000,
-    endTime: '2024-01-17 15:30:00',
-    bid: 1124360,
-    status: 'ING',
-  },
-];
