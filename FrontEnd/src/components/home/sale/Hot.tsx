50356121c44cde24d71bb7e5085cc17992bdb082
이승현 <sh980812@naver.com>
2024-01-31 17:04:36 +0900

    feat: 핫 경매 dto 수정 #S10P12D208-231 #in-progress

--- FrontEnd/src/components/home/sale/Hot.tsx
+++ FrontEnd/src/components/home/sale/Hot.tsx
@@ -1,4 +1,5 @@
-import AuctionItem, { ISaleSimpleRes } from '../AuctionItem';
+import { ISaleSimpleRes } from '@/types/home';
+import AuctionItem from '../AuctionItem';
 
 const Hot = () => {
   return (
@@ -10,7 +11,7 @@ const Hot = () => {
         </div>
         <div className="pl-4 py-4 flex flex-nowrap overflow-x-auto">
           {list.map(item => {
-            return <AuctionItem key={item.salePostSimpleRes.id} item={item} />;
+            return <AuctionItem key={item.dealSimpleRes.id} item={item} />;
           })}
         </div>
       </div>
@@ -22,60 +23,80 @@ export default Hot;
 
 const list: ISaleSimpleRes[] = [
   {
-    salePostSimpleRes: {
+    dealSimpleRes: {
       id: 1,
       title: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
-      categories: 'Digital',
+      category: 'DIGITAL',
       createTime: '2024-01-17 15:30:00',
       image: '',
+      content: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      startTime: '2024-01-17 15:30:00',
     },
-    immediate_price: null,
-    startTime: null,
+    immediate_price: 0,
     startPrice: 100000,
     endTime: '2024-01-17 15:30:00',
     bid: 1124360,
     status: 'AUCTION',
   },
   {
-    salePostSimpleRes: {
+    dealSimpleRes: {
       id: 2,
-      title: '아이폰 삽니다~',
-      categories: 'Digital',
+      title: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      category: 'DIGITAL',
       createTime: '2024-01-17 15:30:00',
       image: '',
+      content: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      startTime: '2024-01-17 15:30:00',
     },
-    immediate_price: null,
-    startTime: null,
+    immediate_price: 0,
     startPrice: 100000,
     endTime: '2024-01-17 15:30:00',
     bid: 1124360,
     status: 'AUCTION',
   },
   {
-    salePostSimpleRes: {
+    dealSimpleRes: {
       id: 3,
-      title: '아이폰 삽니다~',
-      categories: 'Digital',
+      title: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      category: 'DIGITAL',
       createTime: '2024-01-17 15:30:00',
       image: '',
+      content: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      startTime: '2024-01-17 15:30:00',
     },
-    immediate_price: null,
-    startTime: null,
+    immediate_price: 0,
     startPrice: 100000,
     endTime: '2024-01-17 15:30:00',
     bid: 1124360,
     status: 'AUCTION',
   },
   {
-    salePostSimpleRes: {
+    dealSimpleRes: {
       id: 4,
-      title: '아이폰 삽니다~',
-      categories: 'Digital',
+      title: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      category: 'DIGITAL',
       createTime: '2024-01-17 15:30:00',
       image: '',
+      content: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      startTime: '2024-01-17 15:30:00',
     },
-    immediate_price: null,
-    startTime: null,
+    immediate_price: 0,
+    startPrice: 100000,
+    endTime: '2024-01-17 15:30:00',
+    bid: 1124360,
+    status: 'AUCTION',
+  },
+  {
+    dealSimpleRes: {
+      id: 5,
+      title: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      category: 'DIGITAL',
+      createTime: '2024-01-17 15:30:00',
+      image: '',
+      content: '1년도 안쓴 아이폰 15프로 팝니다. 상태좋아요 짱입니다.',
+      startTime: '2024-01-17 15:30:00',
+    },
+    immediate_price: 0,
     startPrice: 100000,
     endTime: '2024-01-17 15:30:00',
     bid: 1124360,
