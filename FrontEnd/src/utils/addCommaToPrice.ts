9d54df254d56daff7b90d601699a1203fb60bab1
이승현 <sh980812@naver.com>
2024-02-01 23:06:15 +0900

    fix: 가격이 0일때 보이지 않는 오류가 있어 수정

--- FrontEnd/src/utils/addCommaToPrice.ts
+++ FrontEnd/src/utils/addCommaToPrice.ts
@@ -1,4 +1,5 @@
 // 가격에 콤마를 추가하는 함수
 export default function addCommaToPrice(price: number | null) {
+  if (price === 0) return 0;
   if (price) return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
