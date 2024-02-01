2d82d094e904ea45af4e11747d013bb1f49f849e
최용수 <cys980220@gmail.com>
2024-02-01 14:24:14 +0900

    fix: redisTemplate 우선순위Bean관련 코드 추가

--- BackEnd/src/main/java/com/qzp/bid/global/config/RedisConfig.java
+++ BackEnd/src/main/java/com/qzp/bid/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.qzp.bid.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -23,6 +24,7 @@ public class RedisConfig {
     }
 
     // setKeySerializer, setValueSerializer 설정으로 redis-cli를 통해 직접 데이터를 보는게 가능하다.
+    @Primary
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
