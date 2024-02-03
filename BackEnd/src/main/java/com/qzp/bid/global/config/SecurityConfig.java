16d17157613600c954736c32af5c824db309908b
차성원 <sungwon4728@naver.com>
2024-02-04 02:48:19 +0900

    fix: 시큐리티 jwtFilter Bean등록

--- BackEnd/src/main/java/com/qzp/bid/global/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/qzp/bid/global/config/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig {
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
+    @Bean
     public JWTFilter jwtFilter() {
         return new JWTFilter(jwtProvider, redisTemplate);
     }
