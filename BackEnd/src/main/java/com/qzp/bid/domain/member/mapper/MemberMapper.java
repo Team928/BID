60f56d401fd5be02682bdc9f678ea39130f90bc8
윤희원 <gmldnjs427@gmail.com>
2024-02-04 01:07:56 +0900

    fix: 회원가입 api 수정 #S10P12D208-136

---
+++ BackEnd/src/main/java/com/qzp/bid/domain/member/mapper/MemberMapper.java
@@ -0,0 +1,20 @@
+package com.qzp.bid.domain.member.mapper;
+
+import com.qzp.bid.domain.member.dto.LoginTokenDto;
+import com.qzp.bid.domain.member.dto.LoginTokenRes;
+import com.qzp.bid.domain.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberMapper {
+
+    @Mapping(source = "member.id", target = "id")
+    @Mapping(source = "member.nickname", target = "nickname")
+    @Mapping(source = "member.area", target = "area")
+    @Mapping(source = "loginTokenDto.accessToken", target = "accessToken")
+    @Mapping(source = "loginTokenDto.refreshToken", target = "refreshToken")
+    LoginTokenRes toLoginTokenRes(Member member, LoginTokenDto loginTokenDto);
+
+}
--- BackEnd/src/main/java/com/qzp/bid/domain/member/service/MemberServiceImpl.java
+++ BackEnd/src/main/java/com/qzp/bid/domain/member/service/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package com.qzp.bid.domain.member.service;
 
+import static com.qzp.bid.global.result.error.ErrorCode.MEMBER_ID_NOT_EXIST;
+
+import com.qzp.bid.domain.member.dto.LoginTokenDto;
+import com.qzp.bid.domain.member.dto.LoginTokenRes;
 import com.qzp.bid.domain.member.dto.MemberJoinReq;
 import com.qzp.bid.domain.member.entity.Member;
 import com.qzp.bid.domain.member.entity.Role;
+import com.qzp.bid.domain.member.mapper.MemberMapper;
 import com.qzp.bid.domain.member.repository.MemberRepository;
+import com.qzp.bid.global.result.error.exception.BusinessException;
 import com.qzp.bid.global.security.util.AccountUtil;
 import com.qzp.bid.global.security.util.JwtProvider;
 import java.util.ArrayList;
@@ -26,17 +32,19 @@ public class MemberServiceImpl implements MemberService {
     private final JwtProvider jwtProvider;
     private final RedisTemplate redisTemplate;
     private final AccountUtil accountUtil;
+    private final MemberMapper memberMapper;
 
     @Override
-    public boolean checkNickname(String nickname) throws Exception {
+    public boolean checkNickname(String nickname) {
         return memberRepository.existsByNickname(nickname);
     }
 
     @Override
     @Transactional
-    public void register(MemberJoinReq memberJoinReq) throws Exception {
+    public LoginTokenRes register(MemberJoinReq memberJoinReq) {
         long memberId = Long.parseLong(accountUtil.getLoginMemberId());
-        Member member = memberRepository.findById(memberId).get();
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new BusinessException(MEMBER_ID_NOT_EXIST));
 
         member.setNickname(memberJoinReq.getNickname());
         member.setArea(new ArrayList<>(Arrays.asList(memberJoinReq.getAddress())));
@@ -46,6 +54,11 @@ public class MemberServiceImpl implements MemberService {
         member.setRole(roles);
 
         memberRepository.save(member);
+
+        LoginTokenDto loginTokenDto = jwtProvider.getLoginResponse(member);
+        LoginTokenRes loginTokenRes = memberMapper.toLoginTokenRes(member, loginTokenDto);
+
+        return loginTokenRes;
     }
 
 }
