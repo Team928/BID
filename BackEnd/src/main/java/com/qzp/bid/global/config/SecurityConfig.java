5791e315d5d6f87098b51db6dab792439dbe2807
차성원 <sungwon4728@naver.com>
2024-02-04 22:19:18 +0900

    fix: 시큐리티 whiteList 하드코딩 제거

--- BackEnd/src/main/java/com/qzp/bid/global/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/qzp/bid/global/config/SecurityConfig.java
@@ -30,8 +30,7 @@ public class SecurityConfig {
     private final RedisTemplate redisTemplate;
     private final JwtProvider jwtProvider;
     @Value("${auth.whiteList}")
-    private String[] whiteList = {"/member/**", "/deals/**", "/chat/**", "/oauth2/**",
-        "/swagger-resources/**", "/swagger-ui/**", "v3/**", "/bid-ui.html", "/api-docs/json/**"};
+    private String[] whiteList;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
