5e9b27914dac5f429f8109da41440382560b1667
최민경 <kjjpyckjs@naver.com>
2024-02-06 21:04:33 +0900

    fear: 이미지 케러셀 컴포넌트 구현 및 메인페이지 적용  #S10P12D208-321

--- FrontEnd/src/pages/home/HomePage.tsx
+++ FrontEnd/src/pages/home/HomePage.tsx
@@ -8,6 +8,10 @@ import { icons } from '@/constants/icons';
 import useTabStore from '@/stores/auctionTabStore';
 import useKeywordStore from '@/stores/keywordStore';
 import { useEffect } from 'react';
+import image1 from '@/assets/image/carouselEx/1.png'; 
+import image2 from '@/assets/image/carouselEx/2.png'; 
+import image3 from '@/assets/image/carouselEx/3.png'; 
+import Carousel from '@/components/@common/Carousel';
 
 const HomePage = () => {
   const { tab } = useTabStore();
@@ -25,6 +29,23 @@ const HomePage = () => {
   useEffect(() => {
     init();
   }, []);
+
+  // 캐러셀 설정
+  const carouselSettings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    autoplay: true, // 자동 캐러셀
+    autoplaySpeed: 1000, // 자동 캐러셀 속도
+  };
+
+  // 캐러셀 이미지 배열
+  const images = [
+    image1,
+    image2,
+    image3,
+  ];
+
   return (
     <>
       <div className="w-full h-screen pb-[4.5rem]">
@@ -32,7 +53,9 @@ const HomePage = () => {
         <AuctionTabBar />
         <div className="pt-[5.1rem] h-full overflow-y-auto">
           {/* #TODO 추후 이미지 찾아서 넣고 위에 글자 넣기 */}
-          <div className="w-full h-1/5 bg-gray-300"></div>
+          <div className="w-full h-1/5 bg-gray-300 mb-4">
+            <Carousel settings={carouselSettings} images={images} />
+          </div>
           <Category />
           {/* 탭에 따른 컴포넌트 보여주기 */}
           {tab === 'sale' ? <SaleTab></SaleTab> : <BuyTab></BuyTab>}
