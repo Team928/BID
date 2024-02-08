bf53c0f5f37af774a5c3d8b330d78b932d0d98c3
이승현 <sh980812@naver.com>
2024-02-08 17:28:36 +0900

    fix: 빌드 오류 수정

--- FrontEnd/src/components/profile/ProfileTabBar.tsx
+++ FrontEnd/src/components/profile/ProfileTabBar.tsx
@@ -2,32 +2,31 @@ import useTabStore from '@/stores/auctionTabStore';
 import React from 'react';
 
 interface ProfileTabBarProps {
-    leftTab: string;
-    rightTab: string;
+  leftTab: string;
+  rightTab: string;
 }
 
 const ProfileTabBar: React.FC<ProfileTabBarProps> = ({ leftTab, rightTab }) => {
-    
-    const { tab, setTab } = useTabStore();
-    
-    return (
-        <div className="w-full flex px-BID_P fixed top-0 pt-16 bg-white z-[9]">
-            <div className="w-1/2 flex justify-center border-b-[3px] py-1 border-BID_MAIN" onClick={() => setTab('sale')}>
-                {tab === 'sale' ? (
-                    <p className="font-bold text-BID_MAIN">{leftTab}</p>
-                ) : (
-                    <p className="text-gray-400 py-1">{leftTab}</p>
-                )}
-            </div>
-            <div className="w-1/2 flex justify-center border-b-[3px] py-1 border-BID_MAIN" onClick={() => setTab('buy')}>
-                {tab === 'buy' ? (
-                    <p className="font-bold text-BID_MAIN">{rightTab}</p>
-                ) : (
-                    <p className="text-gray-400 py-1">{rightTab}</p>
-                )}
-            </div>
-        </div>
-    );
+  const { tab, setTab } = useTabStore();
+
+  return (
+    <div className="w-full flex px-BID_P fixed top-0 pt-16 bg-white z-[9]">
+      <div className="w-1/2 flex justify-center border-b-[3px] py-1 border-BID_MAIN" onClick={() => setTab('sale')}>
+        {tab === 'sale' ? (
+          <p className="font-bold text-BID_MAIN">{leftTab}</p>
+        ) : (
+          <p className="text-gray-400 py-1">{leftTab}</p>
+        )}
+      </div>
+      <div className="w-1/2 flex justify-center border-b-[3px] py-1 border-BID_MAIN" onClick={() => setTab('purchase')}>
+        {tab === 'purchase' ? (
+          <p className="font-bold text-BID_MAIN">{rightTab}</p>
+        ) : (
+          <p className="text-gray-400 py-1">{rightTab}</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProfileTabBar;
