e79fc2c301299b96e0cf731ee0eced4c7c302840
이승현 <sh980812@naver.com>
2024-02-08 17:23:40 +0900

    fix: 소셜 로그인 a태그로 수정

--- FrontEnd/src/pages/login/LoginPage.tsx
+++ FrontEnd/src/pages/login/LoginPage.tsx
@@ -14,12 +14,18 @@ const LoginPage = () => {
             <div className="absolute translate-x-1/2 pl-1">카카오로 시작하기</div>
           </div>
 
-          <Link to="https://i10d208.p.ssafy.io/api/oauth2/authorization/naver">
+          <a href="https://i10d208.p.ssafy.io/api/oauth2/authorization/naver">
             <div className="flex items-center gap-3 border border-[#E4E4E4] p-2 px-BID_P rounded-xl">
               <img src={naver}></img>
               <div className="absolute translate-x-1/2 pl-1">네이버로 시작하기</div>
             </div>
-          </Link>
+          </a>
+          {/* <Link to="https://i10d208.p.ssafy.io/api/oauth2/authorization/naver">
+            <div className="flex items-center gap-3 border border-[#E4E4E4] p-2 px-BID_P rounded-xl">
+              <img src={naver}></img>
+              <div className="absolute translate-x-1/2 pl-1">네이버로 시작하기</div>
+            </div>
+          </Link> */}
         </div>
       </div>
     </>
