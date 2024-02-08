964df73bcf74f01762a239f6a13329a789d14668
이승현 <sh980812@naver.com>
2024-02-08 11:25:49 +0900

    feat: 스크롤 관리하는 observer 커스텀 훅 구현 #S10P12D208-328 #in-progress

---
+++ FrontEnd/src/hooks/@common/useIntersectionObserver.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { InfiniteQueryObserverResult } from '@tanstack/react-query';
+
+//hook props interface
+interface IuseIntersectionObserverProps {
+  threshold?: number;
+  hasNextPage: boolean | undefined;
+  fetchNextPage: () => Promise<InfiniteQueryObserverResult>;
+}
+
+export const useIntersectionObserver = ({
+  threshold = 0.1,
+  hasNextPage,
+  fetchNextPage,
+}: IuseIntersectionObserverProps) => {
+  //관찰할 요소
+  //스크롤 최하단 div요소에 setTarget을 ref로 넣어 사용
+  const [target, setTarget] = useState<HTMLDivElement | null | undefined>(null);
+
+  const observerCallback: IntersectionObserverCallback = entries => {
+    entries.forEach(entry => {
+      //target이 화면에 관찰되고, 다음페이지가 있다면 다음페이지를 호출
+      if (entry.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!target) return;
+
+    //ointersection observer 인스턴스 생성
+    const observer = new IntersectionObserver(observerCallback, {
+      threshold,
+    });
+
+    // 타겟 관찰 시작
+    observer.observe(target);
+
+    // 관찰 멈춤
+    return () => observer.unobserve(target);
+  }, [threshold, target, observerCallback]);
+
+  return { setTarget };
+};
