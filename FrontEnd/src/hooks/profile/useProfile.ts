1cba3a92869be0a29067a6d484ce16d5c1ad0981
이승현 <sh980812@naver.com>
2024-02-09 23:00:45 +0900

    feat: iamport 모의 포인트 충전 구현 및 api 통신 #S10P12D208-330 #done

--- FrontEnd/src/hooks/profile/useProfile.ts
+++ FrontEnd/src/hooks/profile/useProfile.ts
@@ -1,40 +1,65 @@
-import { getBuyListHostReq, getBuyListParticiReq, getSaleListHostReq, getSaleListParticiReq, getUserProfileReq } from "@/service/profile/api"
-import { useQuery } from "@tanstack/react-query"
+import Toast from '@/components/@common/Toast';
+import {
+  getBuyListHostReq,
+  getBuyListParticiReq,
+  getSaleListHostReq,
+  getSaleListParticiReq,
+  getUserProfileReq,
+  postChargePoint,
+} from '@/service/profile/api';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useProfile = () => {
-    const useSaleHost = (nickName: string) => {
-        return useQuery({
-            queryKey: ['sale', 'host', nickName],
-            queryFn: () => getSaleListHostReq(nickName),
-        })
-    }
+  const useSaleHost = (nickName: string) => {
+    return useQuery({
+      queryKey: ['sale', 'host', nickName],
+      queryFn: () => getSaleListHostReq(nickName),
+    });
+  };
 
-    const useSaleParticipant = () => {
-        return useQuery({
-            queryKey: ['saleParticipant'],
-            queryFn: () => getSaleListParticiReq(),
-        })
-    }
+  const useSaleParticipant = () => {
+    return useQuery({
+      queryKey: ['saleParticipant'],
+      queryFn: () => getSaleListParticiReq(),
+    });
+  };
 
-    const useBuyHost = (nickname: string) => {
-        return useQuery({
-            queryKey: ['buy', 'host', nickname],
-            queryFn: () => getBuyListHostReq(nickname),
-        })
-    }
+  const useBuyHost = (nickname: string) => {
+    return useQuery({
+      queryKey: ['buy', 'host', nickname],
+      queryFn: () => getBuyListHostReq(nickname),
+    });
+  };
 
-    const useBuyParticipant = () => {
-        return useQuery({
-            queryKey: ['buyParticipant'],
-            queryFn: () => getBuyListParticiReq(),
-        })
-    }
+  const useBuyParticipant = () => {
+    return useQuery({
+      queryKey: ['buyParticipant'],
+      queryFn: () => getBuyListParticiReq(),
+    });
+  };
 
-    const useUserProfile = (nickname: string) => {
-        return useQuery({
-            queryKey: ['profile', nickname],
-            queryFn: () => getUserProfileReq(nickname),
-        })
-    }
-    return { useSaleHost, useUserProfile, useBuyHost, useSaleParticipant, useBuyParticipant }
-}
+  const useUserProfile = (nickname: string) => {
+    return useQuery({
+      queryKey: ['profile', nickname],
+      queryFn: () => getUserProfileReq(nickname),
+    });
+  };
+
+  const usePostChargePoint = (amount: number, nickname: string) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+      mutationKey: ['point', 'charge', amount],
+      mutationFn: () => postChargePoint(amount),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['profile', nickname] });
+        Toast.success('성공적으로 충전되었습니다');
+      },
+      onError: () => {
+        Toast.error('충전에 실패하였습니다 다시 시도해주세요');
+      },
+    });
+  };
+
+  return { useSaleHost, useUserProfile, useBuyHost, useSaleParticipant, useBuyParticipant, usePostChargePoint };
+};
--- FrontEnd/src/pages/profile/ProfilePage.tsx
+++ FrontEnd/src/pages/profile/ProfilePage.tsx
@@ -6,6 +6,11 @@ import { MdOutlineCreate } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 import userStore from '@/stores/userStore';
 import { useProfile } from '@/hooks/profile/useProfile';
+import { RequestPayParams, RequestPayResponse } from '@/types/model/iamport';
+import Modal from '@/components/@common/Modal';
+import { useState } from 'react';
+import payment_icon from '@/assets/image/payment_icon_yellow_small.png';
+import Toast from '@/components/@common/Toast';
 
 const ProfilePage = () => {
   const info: IHeaderInfo = {
@@ -18,7 +23,7 @@ const ProfilePage = () => {
   const navigate = useNavigate();
 
   const { nickname } = userStore();
-  const { useUserProfile } = useProfile();
+  const { useUserProfile, usePostChargePoint } = useProfile();
   const { data: userProfileInfo } = useUserProfile(`${nickname}`); // 임의 닉네임
 
   const moveToNavigate = (path: string) => {
@@ -32,8 +37,83 @@ const ProfilePage = () => {
     navigate('/login');
   };
 
+  const [showChargeModal, setShowChargeModal] = useState<boolean>(false);
+  const [amount, setAmount] = useState<number>(0);
+
+  const { mutate } = usePostChargePoint(amount, nickname);
+  // 아임포트 결제
+  const handleChargePoint = () => {
+    window.IMP?.init('imp58677553'); // 발급받은 가맹점 식별코드
+
+    if (!amount) {
+      Toast.error('결제 금액을 확인해주세요');
+      return;
+    }
+    const data: RequestPayParams = {
+      pg: 'kakaopay',
+      merchant_uid: `mid_${new Date().getTime()}`,
+      name: '포인트 충전',
+      amount: amount,
+      buyer_name: userProfileInfo?.data.nickname,
+      buyer_email: userProfileInfo?.data.email,
+      // m_redirect_url: '/', 모바일 환경에서는 리다이렉트 url이 필수라 혹시몰라 주석처리
+    };
+
+    const callback = (response: RequestPayResponse) => {
+      const { success } = response;
+      if (success) {
+        console.log(response);
+        mutate();
+        setShowChargeModal(false);
+      } else {
+        console.log(response);
+      }
+    };
+    window.IMP?.request_pay(data, callback);
+  };
+
   return (
     <>
+      {/* 포인트 충전 모달 */}
+      {showChargeModal && (
+        <Modal width="300px" height="auto" title="포인트 충전" onClose={() => setShowChargeModal(false)}>
+          <div className="w-full flex flex-col justify-center items-center px-8 py-2">
+            <div className="w-full">
+              <p className="font-bold">충전 금액</p>
+              <input
+                type="text"
+                name="amount"
+                onChange={e => setAmount(Number(e.target.value))}
+                placeholder="충전할 금액을 입력해주세요"
+                className="w-full outline-none border-b p-2"
+              ></input>
+            </div>
+            <div className="w-full pt-6 pb-2">
+              <p className="font-bold pb-3">결제 수단 선택</p>
+              <div className="flex gap-2">
+                <input type="radio" className="w-4" />
+                <img src={payment_icon} className="h-6" />
+              </div>
+            </div>
+            <div className="w-full flex gap-5 py-5">
+              <button
+                className="w-full bg-BID_SUB_GRAY text-white px-4 py-2 rounded-2xl"
+                onClick={() => setShowChargeModal(false)}
+              >
+                취소
+              </button>
+              <button
+                className="w-full bg-BID_MAIN text-white px-4 py-2 rounded-2xl"
+                onClick={() => {
+                  handleChargePoint();
+                }}
+              >
+                결제
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
       <div>
         <Header info={info} />
         {/* 내 프로필 섹션 */}
@@ -60,7 +140,9 @@ const ProfilePage = () => {
               <p className="text-2xl font-bold">{userProfileInfo?.data.point} P</p>
             </div>
             <div className="flex justify-end px-2 pt-2 text-lg font-bold">
-              <button className="px-2 text-gray-400">충전</button>
+              <button onClick={() => setShowChargeModal(true)} className="px-2 text-gray-400">
+                충전
+              </button>
               <button className="px-2 py-1 text-gray-400">환급</button>
             </div>
           </div>
