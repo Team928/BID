e2373f4792dddc736367e441f61cb645a8d3a3cd
이승현 <sh980812@naver.com>
2024-02-12 21:16:22 +0900

    fix: 네이버 로그인 response 변경

--- FrontEnd/src/components/login/NaverLoginRedirect.tsx
+++ FrontEnd/src/components/login/NaverLoginRedirect.tsx
@@ -1,54 +1,50 @@
-import userStore from "@/stores/userStore";
-import axios from "axios";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
-
+import userStore from '@/stores/userStore';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function NaverLoginRedirect() {
+  const queryParams = new URLSearchParams(location.search);
+  const naverCode = queryParams.get('code'); // 인가 코드
+  const state = queryParams.get('code'); // state 값
+  const navigate = useNavigate();
+  const { loginUser } = userStore();
 
-    const queryParams = new URLSearchParams(location.search);
-    const naverCode = queryParams.get("code"); // 인가 코드
-    const state = queryParams.get("code"); // state 값
-    const navigate = useNavigate();
-    const { loginUser } = userStore();
-    
-    useEffect(() => {
-        console.log(naverCode)
-        if (naverCode) {
-            axios.post(
-                `${import.meta.env.VITE_BASE_URL}auth/login/naver`,
-                { authorizationCode: naverCode, state: state }
+  useEffect(() => {
+    console.log(naverCode);
+    if (naverCode) {
+      axios
+        .post(`${import.meta.env.VITE_BASE_URL}auth/login/naver`, { authorizationCode: naverCode, state: state })
+        .then(res => {
+          console.log(res.data);
 
-            ).then((res) => {
-                console.log(res.data)
-                
-                const { id, accessToken, refreshToken, nickname, area } = res.data
-                const userId = Number(id)
+          const { id, accessToken, refreshToken, nickname, area } = res.data.data;
+          const userId = Number(id);
 
-                if (nickname) {
-                    loginUser({
-                        userId,
-                        accessToken,
-                        refreshToken,
-                        nickname,
-                        area,
-                      });
-                
-                      navigate('/');
-                } else {
-                    loginUser({ accessToken: accessToken, area: '', nickname: '', refreshToken: refreshToken, userId: userId });
-                    navigate('/signup');
-                }
-            })
-            .catch((error) => {
-                console.log('로그인 에러', error)
+          if (nickname) {
+            loginUser({
+              userId,
+              accessToken,
+              refreshToken,
+              nickname,
+              area,
             });
-        }
-    })
 
-    return (
-        <div className="w-screen h-screen flex flex-col justify-center items-center relative">
-            <div>로그인 진행중입니다...</div>
-        </div>
-    )
+            navigate('/');
+          } else {
+            loginUser({ accessToken: accessToken, area: '', nickname: '', refreshToken: refreshToken, userId: userId });
+            navigate('/signup');
+          }
+        })
+        .catch(error => {
+          console.log('로그인 에러', error);
+        });
+    }
+  });
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center relative">
+      <div>로그인 진행중입니다...</div>
+    </div>
+  );
 }
