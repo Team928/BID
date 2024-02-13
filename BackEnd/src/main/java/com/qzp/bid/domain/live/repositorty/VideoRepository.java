1976855458d41ebfd083997c9246424ba042b1ea
최용수 <cys980220@gmail.com>
2024-02-14 03:23:52 +0900

    feat: 비디오영상 가지고 오기 #S10P12D208-340

--- BackEnd/src/main/java/com/qzp/bid/domain/live/repositorty/VideoRepository.java
+++ BackEnd/src/main/java/com/qzp/bid/domain/live/repositorty/VideoRepository.java
@@ -1,10 +1,13 @@
 package com.qzp.bid.domain.live.repositorty;
 
 import com.qzp.bid.domain.live.entity.Video;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
 
     Optional<Video> findByDealId(long dealId);
+
+    List<Video> findAllByDealId(long dealId);
 }
