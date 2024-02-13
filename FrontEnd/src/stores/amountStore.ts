eb322707b6d61c83ca95b2928933c582e31e5654
이승현 <sh980812@naver.com>
2024-02-13 17:35:32 +0900

    fix: iamport 모바일 오류 수정

--- FrontEnd/src/stores/amountStore.ts
+++ FrontEnd/src/stores/amountStore.ts
@@ -1,13 +1,21 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface IAmount {
   amount: number;
   setAmount: (amount: number) => void;
 }
 
-const amountStore = create<IAmount>(set => ({
-  amount: 0,
-  setAmount: newAmount => set({ amount: newAmount }),
-}));
+const amountStore = create(
+  persist<IAmount>(
+    set => ({
+      amount: 0,
+      setAmount: (amount: number) => set({ amount }),
+    }),
+    {
+      name: 'amount-store',
+    },
+  ),
+);
 
 export default amountStore;
