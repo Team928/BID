29495c9e9bace5622d524d3cd43bbddca1fffdcf
이수화 <tnghk9611@naver.com>
2024-02-13 22:00:16 +0900

    feat: 타임라인, 비디오 컴포넌트 구현 #S10P12D208-254 #done

---
+++ FrontEnd/src/components/home/detail/TimeLine.tsx
@@ -0,0 +1,42 @@
+import { useRef, useState } from 'react';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+
+const TimeLine = ({
+  titleContent,
+  children,
+}: {
+  titleContent: string | React.ReactNode;
+  children: string | React.ReactNode;
+}) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    if (!wrapperRef.current || !contentRef.current) return;
+
+    if (!isOpen) {
+      wrapperRef.current.style.height = `${contentRef.current.clientHeight}px`;
+      setIsOpen(true);
+    } else {
+      wrapperRef.current.style.height = '0px';
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <div className="w-full h-full px-2">
+      <div className="py-2 flex justify-between cursor-pointer" onClick={handleOpen}>
+        <div className="text-[13px]">{titleContent}</div>
+        <div>{isOpen ? <IoIosArrowUp size="20" color="#6C6C6C" /> : <IoIosArrowDown size="20" color="#6C6C6C" />}</div>
+      </div>
+      {/* content wrapper */}
+      <div ref={wrapperRef} className="h-0 overflow-hidden transition" style={{ transition: 'all 0.3s ease' }}>
+        <div ref={contentRef}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TimeLine;
