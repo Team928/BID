c13f4912f298b83db790cac13f6248c1b411f6b2
이승현 <sh980812@naver.com>
2024-02-15 09:47:02 +0900

    style: 채팅방 거래 정보 ui 수정

--- FrontEnd/src/components/chat/DealInfo.tsx
+++ FrontEnd/src/components/chat/DealInfo.tsx
@@ -6,26 +6,28 @@ import { useChatLog, useChatRoom } from '@/hooks/chat/useChat';
 import useDealStore from '@/stores/useDealStore';
 import useReviewStore from '@/stores/useReviewStore';
 import userStore from '@/stores/userStore';
+import addCommaToPrice from '@/utils/addCommaToPrice';
 
 const DealInfo = () => {
   const { dealId } = useParams();
   const id = Number(dealId);
 
-  const {userId} = userStore()
+  const { userId } = userStore();
 
   const { useGetChatLogList } = useChatLog();
-  const { useGetChatRoomList } = useChatRoom()
-  const isConfirmed = useDealStore((state) => state.isConfirmed[String(dealId)] || false);
-  const setConfirmed = useDealStore((state) => state.setConfirmed);
+  const { useGetChatRoomList } = useChatRoom();
+  const isConfirmed = useDealStore(state => state.isConfirmed[String(dealId)] || false);
+  const setConfirmed = useDealStore(state => state.setConfirmed);
 
   // 채팅로그 조회
   const { data: chatLogInfo } = useGetChatLogList({ dealId: id });
   // 채팅목록 조회
-  const {data: chatRoomInfo } = useGetChatRoomList({userId: userId})
+  const { data: chatRoomInfo } = useGetChatRoomList({ userId: userId });
 
   const title = chatLogInfo?.data.dealResWithEndPrice.title;
   const content = chatLogInfo?.data.dealResWithEndPrice.content;
   const endPrice = chatLogInfo?.data.dealResWithEndPrice.endPrice;
+  const src = chatLogInfo?.data.dealResWithEndPrice.images[0];
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
@@ -42,58 +44,52 @@ const DealInfo = () => {
   };
 
   const goToReview = () => {
-    navigate(`/review`, { state: { dealInfo: chatLogInfo?.data.dealResWithEndPrice, userInfo: chatRoomInfo?.data} });
-    
+    navigate(`/review`, { state: { dealInfo: chatLogInfo?.data.dealResWithEndPrice, userInfo: chatRoomInfo?.data } });
   };
 
   // reviewPosted 상태 확인
-  const reviewPosted = useReviewStore((state) => state.reviewPosted[String(dealId)] || false);
+  const reviewPosted = useReviewStore(state => state.reviewPosted[String(dealId)] || false);
 
-  console.log(isConfirmed)
-
+  console.log(isConfirmed);
 
   return (
     <div className="fixed top-0 w-full bg-white border-b border-sm">
       <div className="flex gap-4 pt-14 px-4 pb-2 border items-center">
-        <div className="w-20 h-20 bg-BID_LIGHT_GRAY rounded-2xl relative">
+        <div className="w-20 h-20 relative">
+          <img src={`${import.meta.env.VITE_BASE_URL}static${src}`} className="w-full h-full rounded-lg object-cover" />
         </div>
         <div className="flex-1 flex flex-col justify-around">
-          <p className="">{title}</p>
-          <p className="text-sm">{content}</p>
-          <div className='flex items-center'>
-          <p className="text-lg font-bold">{endPrice}</p>
-          <p className="text-xs px-2 text-BID_MAIN">최종 거래가</p>
+          <p className="font-black truncate whitespace-normal line-clamp-2">{title}</p>
+          <p className="text-xs truncate whitespace-normal line-clamp-2 text-black/80">{content}</p>
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-sm">{addCommaToPrice(endPrice!)}원</p>
+            <p className="text-BID_SUB_GRAY text-[10px]">최종 거래가</p>
           </div>
         </div>
         <div>
-
-        {isConfirmed ? (
-          <button
-            className={`text-sm px-3 h-10 ${
-              reviewPosted
-                ? 'text-gray-500 border border-gray-500'
-                : 'text-yellow-500 border border-yellow-500'
-            } rounded-xl font-bold`}
-            onClick={reviewPosted ? undefined : goToReview}
-            disabled={reviewPosted}
-          >
-            {reviewPosted ? '리뷰 작성 완료' : '리뷰 작성'}
-          </button>
-        ) : (
-          <button
-            className="text-sm px-3 h-10 text-BID_MAIN border border-BID_MAIN rounded-xl font-bold"
-            onClick={() => setIsModalOpen(true)}
-          >
-            구매 확정
-          </button>
-        )}
-                
+          {isConfirmed ? (
+            <button
+              className={`text-sm px-3 h-10 ${
+                reviewPosted ? 'text-gray-500 border border-gray-500' : 'text-yellow-500 border border-yellow-500'
+              } rounded-xl font-bold`}
+              onClick={reviewPosted ? undefined : goToReview}
+              disabled={reviewPosted}
+            >
+              {reviewPosted ? '리뷰 작성 완료' : '리뷰 작성'}
+            </button>
+          ) : (
+            <button
+              className="text-sm px-3 h-10 text-BID_MAIN border border-BID_MAIN rounded-xl font-bold"
+              onClick={() => setIsModalOpen(true)}
+            >
+              구매 확정
+            </button>
+          )}
         </div>
       </div>
 
       {/* 모달 영역 */}
-      {isModalOpen && <ConfirmModal onClose={() => setIsModalOpen(false)} onConfirm={handleConfirm}/>
-      }
+      {isModalOpen && <ConfirmModal onClose={() => setIsModalOpen(false)} onConfirm={handleConfirm} />}
     </div>
   );
 };
